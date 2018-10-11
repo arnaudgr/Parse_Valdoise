2d0a93814587078416abd184b3e221f3d29371ff
jango974 <votre@email.com>
2018-10-11 18:06:54 +0200

    email et url

--- Val_d'oise Mairie.rb
+++ Val_d'oise Mairie.rb
@@ -3,17 +3,32 @@ require 'nokogiri'
 require'open-uri'
 require 'pry'
 
-require 'nokogiri'
-require 'open-uri'
 
-doc = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/vaureal.html"))
 
+
+
+
+def get_the_email_of_a_townhal_from_its_webpage	
+page = Nokogiri::HTML(open('http://annuaire-des-mairies.com/95/vaureal.html'))
+adresses = page.css ("td")
+puts adresses[7].text
+end
+puts get_the_email_of_a_townhal_from_its_webpage	
+
+
+def get_all_the_urls_of_val_doise_townhalls
+page = Nokogiri::HTML(open('http://annuaire-des-mairies.com/val-d-oise.html'))
+liens = page.css ("a")
+liens.each{|liens| puts liens ['href']}
+end
+puts get_all_the_urls_of_val_doise_townhalls
 
  	
-   domain_page = doc.xpath ('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]')
+   
    
 
-css = 'table.table:nth-child(1) > tbody:nth-child(3) > tr:nth-child(4) > td:nth-child(2)'
 
-doc.css(css).each do |email|
-  puts email
+
+
+
+   
